c1d890e8d6610d669c12dac695e96ad76ba00d50
Chip Lempke <clempke90@gmail.com>
2020-12-05 23:35:46 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,7 @@ def find_even_values(num)
   row_index = 0
   while row_index < num.count do
     column_index = 0
-    while column_index < num[row_index].count do
+    while column_index < num[column_index].count do
       if num[column_index][element_index].even?
         p num[column_index][element_index]
       end
